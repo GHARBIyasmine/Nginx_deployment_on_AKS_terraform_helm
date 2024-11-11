f4de856f3703f89974e9081ca07d14cd36c1ba9b
Yasmine Gharbi <99125118+GHARBIyasmine@users.noreply.github.com>
2024-11-11 19:44:42 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 # main.tf
 
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
